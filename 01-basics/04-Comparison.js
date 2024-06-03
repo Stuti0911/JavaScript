b16f5d5e1af1d85b5f8bca2be92ca88cbe5dc589
Stuti Goel <123297321+Stuti0911@users.noreply.github.com>
2024-06-03 07:35:08 +0000

    comparison doen

---
+++ 01-basics/04-Comparison.js
@@ -0,0 +1,29 @@
+// console.log(2>1);
+
+//comparing diff datatypes
+// 
+// console.log(Number("01")>1);
+// console.log("03">1);
+// console.log("1">1);
+// console.log("01"==1);
+
+//check == , comparisons > < >= <= work differntly 
+// comparison convert num to number treating it as 0
+
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0);
+
+// all false values
+console.log(undefined ==0 );
+console.log(undefined>0);
+console.log(undefined>=0);
+
+// === strict check along with datatype
+console.log("2"==2);    //true
+console.log("2"===2); //false
+
+
+
+
+
