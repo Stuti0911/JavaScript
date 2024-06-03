5d9b1a2c27135763e9823a4b11fce0b0991020b4
Stuti Goel <123297321+Stuti0911@users.noreply.github.com>
2024-06-03 07:24:47 +0000

    opetarion changes

--- 01-basics/03-ConversionOp.js
+++ 01-basics/03-ConversionOp.js
@@ -1,13 +1,13 @@
 let score= null
 
-//01-basics/03-ConversionOp.js
+//node 01-basics/03-ConversionOp.js
 
-console.log(typeof score);
+//console.log(typeof score);
 // console.log(typeof ( score)); ANOTHER WAY
 
 let scoreInNum= Number(score);
-console.log(typeof scoreInNum);
-console.log(scoreInNum);  //22abc number m convert nahi hoga-->NaN
+// console.log(typeof scoreInNum);
+// console.log(scoreInNum);  //22abc number m convert nahi hoga-->NaN
 
 //Converting such string into number
 // "33"->Number done value:33
@@ -21,3 +21,34 @@ let logg= Boolean(1)
 //1=> true 0=> false
 // ""->false
 //"some_value"=>true
+
+
+//--------------Operations-----------
+
+let val=3
+let negVal=-val;
+
+//console.log(negVal);
+
+let st1="hello this is my 2nd day"
+let str=" keep going"
+let str3= st1+ str
+// console.log(str3)
+
+// console.log(2+1)
+// console.log(2+"1")
+// console.log("2"+"1")
+// console.log(2+1+1)
+// console.log("1"+2+2)
+// console.log(2+2+"1")
+
+// yeh krna nahi chahiye 
+// console.log(true);
+// console.log(+true);  //true-->1
+// console.log(+true+false+true);
+// console.log(+false);
+//console.log(+'')
+
+//If used postfix, with operator after operand (for example, x++), the increment operator increments and returns the value before incrementing.
+
+//If used prefix, with operator before operand (for example, ++x), the increment operator increments and returns the value after incrementing.
