351d63bf3c6a468802860bc6526e632cc218de7e
Stuti Goel <123297321+Stuti0911@users.noreply.github.com>
2024-06-04 07:51:14 +0000

    object

---
+++ 02-basics/03-object.js
@@ -0,0 +1,16 @@
+// object declare 2 ways, literal(not singleton) and constructor(singleton)
+
+//object constructor object.create()
+//object literals
+
+const user={
+    name:"stu",
+    age:20,
+    location:"jaipur",
+    email:"stuti@gmail.com",
+    isLoggedIn:false,
+    lastLoggedinDays:["monday","wednesday"]
+}
+
+console.log(user.name);
+console.log(user[name]);
