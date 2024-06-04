d096813437a653477058b26b202ea457968d6d23
Stuti Goel <123297321+Stuti0911@users.noreply.github.com>
2024-06-04 08:18:17 +0000

    objt

---
+++ 02-basics/03-object.js
@@ -0,0 +1,15 @@
+// // object declare: constructor(singleton) , literals(non-singleton)
+
+// //Object.create // constructor
+
+// const user={
+//     name:"stuti",
+//     age:20,
+//     location:"jaipur",
+//     email:"stuti@gmail.com",
+//     isLoggedIn:false,
+//     lastLoggedIn: ["mon","tues","wedns"]
+// }
+
+// console.log(user.email)
+console.log("heelo");
