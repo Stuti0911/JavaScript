2ecb9f9559280899d0bda81143eb7d1ebc756dc3
Stuti Goel <123297321+Stuti0911@users.noreply.github.com>
2024-06-04 11:19:28 +0000

    object part3

---
+++ 02-basics/05-object.js
@@ -0,0 +1,22 @@
+const course={
+    name:"js",
+    price:"999",
+    instructor: "stuti"
+}
+
+//course.name
+//Destructuring
+//const {instructor : ins}=course // u can now address instructor as ins only
+//console.log(ins);
+
+console.log(instructor);
+
+//API backend se values in JSON format
+// {
+//     "name":"stuti",
+//     "courseName":"jss",
+//     "price":"free"
+// }
+// [  array format Api
+//     {},{},{}
+// ]
