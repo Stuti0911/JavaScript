4d613a3603277b29a386e2ec292db340d55b0af9
Stuti Goel <123297321+Stuti0911@users.noreply.github.com>
2024-06-05 08:48:20 +0000

    loop

---
+++ 05-Loops/one.js
@@ -0,0 +1,24 @@
+//Multiple cursors ctrl+D
+//Copy: alt+shift+down arrow
+// for (let index = 0; index <10; index++) {
+//     console.log(index);   
+// }
+
+let index=0
+// while (index<=10) {
+//     console.log(index);
+//     index++
+// }
+//will execute atleast once
+let score=1;
+do {
+    console.log(`score is ${score}`);
+    score++;
+} while (score<=10);
+
+
+
+
+
+
+
