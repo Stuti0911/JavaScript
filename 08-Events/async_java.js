1c514831165dcce062019d3fbaa4c689bd734ad6
Stuti Goel <123297321+Stuti0911@users.noreply.github.com>
2024-06-09 18:39:57 +0000

    Asynchronous JavaScript done

---
+++ 08-Events/async_java.js
@@ -0,0 +1,11 @@
+/* 
+    Javascript: syn=> line by line code 
+    single threaded
+
+    Blocking Code                 v/s       Non blocking code
+    block the flow of the program           Don't block execution
+    Read file sync                          Read file Async
+    
+
+*/
+
